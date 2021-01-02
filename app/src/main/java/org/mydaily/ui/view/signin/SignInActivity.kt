9763c12436518e3455eaf388a1705439f268640f
Seulgi Kim <7d4z7l@gmail.com>
2021-01-02 15:41:39 +0900

    [ADD] #9 - add SignInActivity & initiate view

---
+++ app/src/main/java/org/mydaily/ui/view/signin/SignInActivity.kt
@@ -0,0 +1,48 @@
+package org.mydaily.ui.view.signin
+
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.mydaily.R
+import org.mydaily.databinding.ActivitySignInBinding
+import org.mydaily.ui.base.BaseActivity
+import org.mydaily.ui.viewmodel.SignInViewModel
+import org.mydaily.util.extension.shortToast
+
+class SignInActivity : BaseActivity<ActivitySignInBinding, SignInViewModel>() {
+
+    override val layoutResourceId: Int
+        get() = R.layout.activity_sign_in
+    override val viewModel: SignInViewModel by viewModel()
+
+    override fun initView() {
+        initClickEvent()
+    }
+
+    override fun initBeforeBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+
+    private fun initClickEvent() {
+        binding.ibAutoLogin.isSelected = true
+        binding.ibAutoLogin.setOnClickListener {
+            it.isSelected = !it.isSelected
+            shortToast(this, "자동로그인 버튼 클릭"+it.isSelected)
+        }
+        binding.tvFindId.setOnClickListener {
+            shortToast(this, "아이디찾기 버튼 클릭")
+        }
+        binding.tvFindPassword.setOnClickListener {
+            shortToast(this, "비번찾기 버튼 클릭")
+        }
+        binding.tvSignUp.setOnClickListener {
+            shortToast(this, "회원가입 버튼 클릭")
+        }
+        binding.btnSignIn.setOnClickListener {
+            shortToast(this, "로그인 버튼 클릭")
+            //viewModel.signIn(binding.etEmail.toString(), binding.etPassword.toString())
+        }
+    }
+}
---
+++ app/src/main/java/org/mydaily/ui/viewmodel/SignInViewModel.kt
@@ -0,0 +1,11 @@
+package org.mydaily.ui.viewmodel
+
+import org.mydaily.ui.base.BaseViewModel
+
+class SignInViewModel: BaseViewModel() {
+
+    fun signIn(email: String, password: String) {
+
+
+    }
+}
