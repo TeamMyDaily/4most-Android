edbca93e774238132aa2186cc19a7a6edb8b2ef0
Seulgi Kim <7d4z7l@gmail.com>
2021-01-02 15:53:22 +0900

    [MOD] use kotlin extension functions when to show toast

--- app/src/main/java/org/mydaily/ui/view/signin/SignInActivity.kt
+++ app/src/main/java/org/mydaily/ui/view/signin/SignInActivity.kt
@@ -30,23 +30,23 @@ class SignInActivity : BaseActivity<ActivitySignInBinding, SignInViewModel>() {
         binding.ibAutoLogin.isSelected = true
         binding.ibAutoLogin.setOnClickListener {
             it.isSelected = !it.isSelected
-            shortToast(this, "자동로그인 버튼 클릭" + it.isSelected)
+            shortToast("자동로그인 버튼 클릭" + it.isSelected)
         }
         binding.tvFindId.setOnClickListener {
-            shortToast(this, "아이디찾기 버튼 클릭")
+            shortToast("아이디찾기 버튼 클릭")
         }
         binding.tvFindPassword.setOnClickListener {
-            shortToast(this, "비번찾기 버튼 클릭")
+            shortToast("비번찾기 버튼 클릭")
         }
         binding.tvSignUp.setOnClickListener {
-            shortToast(this, "회원가입 버튼 클릭")
+            shortToast("회원가입 버튼 클릭")
         }
         binding.btnSignIn.setOnClickListener {
             val email = binding.etPassword.toString()
             val password = binding.etPassword.toString()
 
             if (LoginPatternCheckUtil.isNotValidEmailAndPassword(email, password)) {
-                shortToast(this, R.string.msg_sign_in_error)
+                shortToast(R.string.msg_sign_in_error)
             }else {
                 viewModel.signIn(email, password)
             }
