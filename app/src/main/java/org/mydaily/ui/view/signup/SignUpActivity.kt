e2f3dea856512c7db5abb9ec86257fd55744b489
Seulgi Kim <7d4z7l@gmail.com>
2021-01-04 09:59:18 +0900

    [ADD] #12 - SignUpActivity & textinputlayout

---
+++ app/src/main/java/org/mydaily/ui/view/signup/SignUpActivity.kt
@@ -0,0 +1,26 @@
+package org.mydaily.ui.view.signup
+
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.mydaily.R
+import org.mydaily.databinding.ActivitySignUpBinding
+import org.mydaily.ui.base.BaseActivity
+import org.mydaily.ui.viewmodel.SignUpViewModel
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding, SignUpViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_sign_up
+    override val viewModel: SignUpViewModel by viewModel()
+
+    override fun initView() {
+
+    }
+
+    override fun initBeforeBinding() {
+
+    }
+
+    override fun initAfterBinding() {
+
+    }
+
+}
