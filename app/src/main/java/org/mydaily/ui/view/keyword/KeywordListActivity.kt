560a2a1034aa35480c382de1159650cb847dc6f9
Jihwan <sgh002400@naver.com>
2021-01-05 17:31:40 +0900

    [ADD] #5 - add Manifest.xml, colors.xml, strings.xml, Activity+xml

--- app/src/main/java/org/mydaily/ui/view/keyword/KeywordListActivity.kt
+++ app/src/main/java/org/mydaily/ui/view/keyword/KeywordListActivity.kt
@@ -1,12 +1,27 @@
 package org.mydaily.ui.view.keyword
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_keyword_list.*
 import org.mydaily.R
 
 class KeywordListActivity : AppCompatActivity() {
+    val count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_keyword_list)
+        setContentView(R.layout.activity_main)
+
+//        btn_add_word.setOnClickListener{
+//            val chipgroup3 = cg_add
+//            val chip = Chip(this)
+//            chip.text = "hi"
+//            chip.setTextColor(getColor(R.color.white))
+//            chip.setChipBackgroundColorResource(R.color.chip)
+
+//            //xml처럼 style을 지정해줘야 하는데 어떻게 하는지 모르겠음.
+//            //style="@style/Widget.MaterialComponents.Chip.Choice"
+//            chipgroup3.addView(chip)as
+//        }
     }
 }
