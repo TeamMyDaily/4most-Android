196cd6c1b4c0b9b00ebf960a1497d52014dc4b60
Seulgi Kim <7d4z7l@gmail.com>
2021-01-05 10:21:31 +0900

    [ADD] #17 - add daily detail activity and initial view setting

--- app/src/main/java/org/mydaily/ui/view/daily/detail/DailyDetailActivity.kt
+++ app/src/main/java/org/mydaily/ui/view/daily/detail/DailyDetailActivity.kt
@@ -2,11 +2,13 @@ package org.mydaily.ui.view.daily.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mydaily.R
 import org.mydaily.databinding.ActivityDailyDetailBinding
 import org.mydaily.ui.base.BaseActivity
 import org.mydaily.ui.viewmodel.DailyViewModel
+import org.mydaily.util.extension.shortToast
 
 class DailyDetailActivity : BaseActivity<ActivityDailyDetailBinding, DailyViewModel>() {
     override val layoutResourceId: Int
@@ -14,7 +16,8 @@ class DailyDetailActivity : BaseActivity<ActivityDailyDetailBinding, DailyViewMo
     override val viewModel: DailyViewModel by viewModel()
 
     override fun initView() {
-
+        getIntentData()
+        initToolbar()
     }
 
     override fun initBeforeBinding() {
@@ -22,6 +25,26 @@ class DailyDetailActivity : BaseActivity<ActivityDailyDetailBinding, DailyViewMo
     }
 
     override fun initAfterBinding() {
+
+    }
+
+    private fun getIntentData() {
+        val num = intent.getIntExtra("keyword",0)
+        shortToast("$num 번째 키워드 클릭해서 들어옴")
+
+        binding.tvKeyword.text = "열정"+num
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.tbDailyDetail)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding.tvTitle.text = "기록"
+        binding.tbDailyDetail.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initIntentData() {
 
     }
 
