c4c8ab442a1b40ed1b1edc8a0a8c7666bd999a4d
Jihwan <sgh002400@naver.com>
2021-01-05 16:44:57 +0900

    [ADD] #15 - add colors.xml, strings.xml, activity_keyword_add.xml

---
+++ app/src/main/java/org/mydaily/ui/view/keyword/KeywordAddActivity.kt
@@ -0,0 +1,12 @@
+package org.mydaily.ui.view.keyword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mydaily.R
+
+class KeywordAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keyword_add)
+    }
+}
