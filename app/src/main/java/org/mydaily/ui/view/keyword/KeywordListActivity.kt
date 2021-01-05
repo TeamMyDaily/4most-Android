059b21311e3c55da912c6bfe680356a94eb103d5
Jihwan <sgh002400@naver.com>
2021-01-05 17:13:38 +0900

    [ADD] #15 - add gradle, add activity_keyword_list.xml, add strings.xml, selector_chip.xml

---
+++ app/src/main/java/org/mydaily/ui/view/keyword/KeywordListActivity.kt
@@ -0,0 +1,12 @@
+package org.mydaily.ui.view.keyword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mydaily.R
+
+class KeywordListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keyword_list)
+    }
+}
