0bf93c49682725791c91f6575a0896f032902530
Jihwan <sgh002400@naver.com>
2021-01-09 18:02:42 +0900

    [ADD] #15 - Stop typing english, special character (~ing)

--- app/src/main/java/org/mydaily/ui/view/keyword/KeywordAddActivity.kt
+++ app/src/main/java/org/mydaily/ui/view/keyword/KeywordAddActivity.kt
@@ -1,17 +1,17 @@
 package org.mydaily.ui.view.keyword
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
-import kotlinx.android.synthetic.main.activity_keyword_add.*
+import android.view.View
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mydaily.R
 import org.mydaily.databinding.ActivityKeywordAddBinding
-import org.mydaily.databinding.ActivityKeywordListBinding
 import org.mydaily.ui.base.BaseActivity
 import org.mydaily.ui.viewmodel.KeywordViewModel
 import org.mydaily.util.extension.shortToast
+import java.util.regex.Pattern
+
 
 class KeywordAddActivity : BaseActivity<ActivityKeywordAddBinding, KeywordViewModel>() {
     override val layoutResourceId: Int
@@ -22,8 +22,20 @@ class KeywordAddActivity : BaseActivity<ActivityKeywordAddBinding, KeywordViewMo
 
     override fun initView() {
         initToolbar()
+        initEditText()
         keywordInput()
         keywordAdd()
+    }
+
+    private fun initEditText() {
+        val filterKorea = InputFilter { source, start, end, dest, dstart, dend ->
+            val ps = Pattern.compile(getString(R.string.korean_keyboard))
+            if (!ps.matcher(source).matches()) {
+                ""
+            } else null
+        }
+        //TODO -> 유니코드 수정해서 a 입력 안되게 막기
+        binding.etKeywordInput.filters = arrayOf(filterKorea)
     }
 
     override fun initBeforeBinding() {
@@ -65,35 +77,50 @@ class KeywordAddActivity : BaseActivity<ActivityKeywordAddBinding, KeywordViewMo
         }
 
     }
-
     private fun keywordInput() {
         var sameKeyword : String
-        et_keyword_input.addTextChangedListener(object : TextWatcher {
+        binding.etKeywordInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(et_keyword_input.text.toString().isEmpty()) {
-                    btn_add.isEnabled = false
-                    et_keyword_input.error = "비어있다!"
-                } else if(et_keyword_input.length() > 5) {
-                    btn_add.isEnabled = false
-                    et_keyword_input.error = "5글자 초과!"
-                } else if(et_keyword_input.text.toString().contains(" ")) {
-                    btn_add.isEnabled = false
-                    et_keyword_input.error = "공백이다!"
-                } else if(keywordList.contains(et_keyword_input.text.toString())) {
-                    btn_add.isEnabled = false
-                    sameKeyword = et_keyword_input.text.toString()
-                    et_keyword_input.error = "$sameKeyword 잡았다 요놈!!"
+                if (binding.etKeywordInput.text.toString().isEmpty()) {
+                    binding.btnAdd.isEnabled = false
+                } else if (binding.etKeywordInput.length() > 5) {
+                    binding.btnAdd.isEnabled = false
+                    binding.tvErrortext.visibility = View.VISIBLE
+                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.tvErrortext.setText(R.string.maximum_five_word)
+                } else if (binding.etKeywordInput.text.toString().contains(" ")) {
+                    binding.btnAdd.isEnabled = false
+                    binding.tvErrortext.visibility = View.VISIBLE
+                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.tvErrortext.setText(R.string.there_is_space)
+                } else if (keywordList.contains(binding.etKeywordInput.text.toString())) {
+                    binding.btnAdd.isEnabled = false
+                    sameKeyword =
+                        "'" + binding.etKeywordInput.text.toString() + "' 은(는) " + getString(R.string.already_exist)
+                    binding.tvErrortext.visibility = View.VISIBLE
+                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.tvErrortext.text = sameKeyword
+                } else if(binding.etKeywordInput.text.toString().contains("a")) {
+                    //TODO -> 알파벳이랑 특수문자 입력됐을 때 공백 들어갔을 때처럼 로직 작성하기
+                    binding.btnAdd.isEnabled = false
+                    binding.tvErrortext.visibility = View.VISIBLE
+                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.tvErrortext.setText("영어존재")
+
                 } else {
-                    btn_add.isEnabled = true
+                    binding.btnAdd.isEnabled = true
+                    binding.tvErrortext.visibility = View.GONE
+                    binding.btnErrorIcon.visibility = View.GONE
                 }
             }
+
             override fun afterTextChanged(s: Editable?) {}
         })
     }
     private fun keywordAdd() {
-        btn_add.setOnClickListener{
+        binding.btnAdd.setOnClickListener{
             //TODO 추가 로직 작성
             shortToast("키워드 추가 버튼 클릭됨")
             finish()
