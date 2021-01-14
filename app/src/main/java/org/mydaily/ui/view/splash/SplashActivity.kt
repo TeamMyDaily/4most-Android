daed4384f32d90b3d227e67853d3bbfd3263bbcf
Jihwan <sgh002400@naver.com>
2021-01-14 23:45:03 +0900

    [ADD] 77 - add ServiceExplainActivity, SplashActivity

---
+++ app/src/main/java/org/mydaily/ui/view/splash/SplashActivity.kt
@@ -0,0 +1,45 @@
+package org.mydaily.ui.view.splash
+
+import android.content.Intent
+import android.graphics.drawable.Animatable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import com.airbnb.lottie.LottieDrawable
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.koin.android.ext.android.bind
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.mydaily.R
+import org.mydaily.databinding.ActivityServiceExplainBinding
+import org.mydaily.databinding.ActivitySplashBinding
+import org.mydaily.ui.adapter.KeywordPopupViewPagerAdapter
+import org.mydaily.ui.base.BaseActivity
+import org.mydaily.ui.view.MainActivity
+import org.mydaily.ui.view.keyword.popup.KeywordPopupFirstFragment
+import org.mydaily.ui.view.keyword.popup.KeywordPopupSecondFragment
+import org.mydaily.ui.view.keyword.popup.KeywordPopupThirdFragment
+import org.mydaily.ui.viewmodel.KeywordViewModel
+
+class SplashActivity : BaseActivity<ActivitySplashBinding, KeywordViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_splash
+    override val viewModel: KeywordViewModel by viewModel()
+    private val SPLASH_VIEW_TIME: Long = 2200
+    override fun initView() {
+        initLottie()
+    }
+
+    override fun initBeforeBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+    private fun initLottie() {
+        binding.splash.setAnimation("splash.json")
+        binding.splash.repeatMode = LottieDrawable.REVERSE
+        binding.splash.repeatCount = LottieDrawable.INFINITE
+        binding.splash.playAnimation()
+    }
+}
---
+++ app/src/main/java/org/mydaily/ui/view/splash/service/ServiceExplainActivity.kt
@@ -0,0 +1,95 @@
+package org.mydaily.ui.view.splash.service
+
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.mydaily.R
+import org.mydaily.databinding.ActivityServiceExplainBinding
+import org.mydaily.ui.adapter.ServiceExplainViewPagerAdapter
+import org.mydaily.ui.base.BaseActivity
+import org.mydaily.ui.view.keyword.popup.KeywordPopupFirstFragment
+import org.mydaily.ui.view.keyword.popup.KeywordPopupSecondFragment
+import org.mydaily.ui.view.keyword.popup.KeywordPopupThirdFragment
+import org.mydaily.ui.viewmodel.KeywordViewModel
+
+class ServiceExplainActivity : BaseActivity<ActivityServiceExplainBinding, KeywordViewModel>() {
+    override val layoutResourceId: Int
+        get() = R.layout.activity_service_explain
+
+    private lateinit var viewpagerAdapter: ServiceExplainViewPagerAdapter
+
+    override val viewModel: KeywordViewModel by viewModel()
+
+    override fun initView() {
+        initBtnVisibility()
+        initViewPager()
+        initTabLayout()
+        viewPagerChange()
+        btnExplainFinishClicked()
+        tvExplainSkipClicked()
+    }
+
+    override fun initBeforeBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+    private fun btnExplainFinishClicked() {
+        binding.btnExplainFinish.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun tvExplainSkipClicked() {
+        binding.tvExplainSkip.setOnClickListener {
+            binding.vpServiceExplain.currentItem = 2
+        }
+    }
+
+    private fun initViewPager() {
+        viewpagerAdapter = ServiceExplainViewPagerAdapter(supportFragmentManager)
+        viewpagerAdapter.fragments = listOf(
+            KeywordPopupFirstFragment(),
+            KeywordPopupSecondFragment(),
+            KeywordPopupThirdFragment()
+        )
+        binding.vpServiceExplain.adapter = viewpagerAdapter
+    }
+
+    private fun initBtnVisibility() {
+        binding.btnExplainFinish.visibility = View.GONE
+        binding.tvExplainSkip.visibility = View.VISIBLE
+    }
+
+    private fun initTabLayout() {
+        binding.tlSplash.setupWithViewPager(binding.vpServiceExplain)
+    }
+
+    private fun viewPagerChange() {
+        binding.vpServiceExplain.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                btnOnOff()
+            }
+
+            override fun onPageSelected(position: Int) {
+                btnOnOff()
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+    }
+
+    private fun btnOnOff() {
+        if (binding.vpServiceExplain.currentItem == 2) {
+            binding.tvExplainSkip.visibility = View.GONE
+            binding.btnExplainFinish.visibility = View.VISIBLE
+        }
+    }
+}
