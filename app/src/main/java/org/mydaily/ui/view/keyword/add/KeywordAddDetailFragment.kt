161bb6eeeb84151afacdb2a7da733cf83f312674
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-03-12 19:58:54 +0900

    [REFACTOR] #100 - delete useless code and refactor

--- app/src/main/java/org/mydaily/ui/view/keyword/add/KeywordAddDetailFragment.kt
+++ app/src/main/java/org/mydaily/ui/view/keyword/add/KeywordAddDetailFragment.kt
@@ -1,21 +1,14 @@
 package org.mydaily.ui.view.keyword.add
 
-import android.content.Intent
-import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.mydaily.R
-import org.mydaily.databinding.FragmentKeywordAddBinding
 import org.mydaily.databinding.FragmentKeywordAddDetailBinding
 import org.mydaily.ui.base.BaseFragment
 import org.mydaily.ui.viewmodel.KeywordViewModel
+import org.mydaily.util.KoreanInWordCheckUtil
 import org.mydaily.util.extension.popBackStack
-import java.util.regex.Pattern
 
 class KeywordAddDetailFragment : BaseFragment<FragmentKeywordAddDetailBinding, KeywordViewModel>() {
     override val layoutResourceId: Int
@@ -41,50 +34,40 @@ class KeywordAddDetailFragment : BaseFragment<FragmentKeywordAddDetailBinding, K
 
     private fun keywordInput() {
         var sameKeyword: String
+        binding.isNotVisible = true
         binding.etKeywordInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (binding.etKeywordInput.text.toString().isEmpty()) {
                     binding.btnAdd.isEnabled = false
-                    binding.tvErrorText.visibility = View.INVISIBLE
-                    binding.btnErrorIcon.visibility = View.INVISIBLE
+                    binding.isNotVisible = true
                 } else if (binding.etKeywordInput.length() > 5) {
                     binding.btnAdd.isEnabled = false
-                    binding.tvErrorText.visibility = View.VISIBLE
-                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.isNotVisible = false
                     binding.tvErrorText.setText(R.string.maximum_five_word)
                 } else if (binding.etKeywordInput.text.toString().contains(" ")) {
                     binding.btnAdd.isEnabled = false
-                    binding.tvErrorText.visibility = View.VISIBLE
-                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.isNotVisible = false
                     binding.tvErrorText.setText(R.string.there_is_space)
                 } else if (addedMyWord.contains(binding.etKeywordInput.text.toString())) {
                     binding.btnAdd.isEnabled = false
                     sameKeyword =
                         "'" + binding.etKeywordInput.text.toString() + "' 은(는) " + getString(R.string.already_exist)
-                    binding.tvErrorText.visibility = View.VISIBLE
-                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.isNotVisible = false
                     binding.tvErrorText.text = sameKeyword
-                } else if (isKoreanInWord(binding.etKeywordInput.text.toString())) {
+                } else if (KoreanInWordCheckUtil.isKoreanInWord(binding.etKeywordInput.text.toString())) {
                     binding.btnAdd.isEnabled = false
-                    binding.tvErrorText.visibility = View.VISIBLE
-                    binding.btnErrorIcon.visibility = View.VISIBLE
+                    binding.isNotVisible = false
                     binding.tvErrorText.text = "특수문자, 숫자, 영어를 제외하고 입력해주세요."
                 } else {
                     binding.btnAdd.isEnabled = true
-                    binding.tvErrorText.visibility = View.GONE
-                    binding.btnErrorIcon.visibility = View.GONE
+                    binding.isNotVisible = true
                 }
             }
 
             override fun afterTextChanged(s: Editable?) {}
         })
-    }
-
-    private fun isKoreanInWord(keyword: String): Boolean {
-        val keywordPattern: Pattern = Pattern.compile(getString(R.string.korean_keyboard))
-        return !keywordPattern.matcher(keyword).matches()
     }
 
     private fun initToolbar() {
--- app/src/main/java/org/mydaily/ui/view/keyword/add/KeywordAddFragment.kt
+++ app/src/main/java/org/mydaily/ui/view/keyword/add/KeywordAddFragment.kt
@@ -1,23 +1,12 @@
 package org.mydaily.ui.view.keyword.add
 
-import android.content.Intent
-import android.opengl.Visibility
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mydaily.R
 import org.mydaily.databinding.FragmentKeywordAddBinding
 import org.mydaily.ui.adapter.KeywordAddAdapter
-import org.mydaily.ui.adapter.KeywordDefineAdapter
 import org.mydaily.ui.base.BaseFragment
-import org.mydaily.ui.view.keyword.guide.KeywordGuideSelectFragment
-import org.mydaily.ui.view.remind.ReportDetailFragment
 import org.mydaily.ui.viewmodel.KeywordViewModel
 import org.mydaily.util.extension.popBackStack
 import org.mydaily.util.extension.replaceAndAddBackStack
